f944ff4a0f197cc27b53d025029a4b7b796d1976
headmasterritual <thebiggestapple007@gmail.com>
2022-03-06 02:28:06 +0200

    rere

---
+++ src/test/java/tests/basetest.java
@@ -0,0 +1,11 @@
+package tests;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class basetest {
+
+}
--- src/test/java/tests/task1.java
+++ src/test/java/tests/task1.java
@@ -1,27 +1,34 @@
+package tests;
+
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("Api tests")
-class TestImplementation {
-
-//    @Test
-    void openTest() {
+public class task1 extends basetest{
+    @BeforeAll
+    public static void init() {
         RestAssured.baseURI = "http://localhost";
-        RestAssured.basePath = "";
+//		RestAssured.port=8080; //commented as our API doesnot use port
         RestAssured.proxy("localhost", 3000);
+    }
+    @Test
+    void task1() {
         given().accept(ContentType.JSON)
                 .filter(new AllureRestAssured())
-                .when().get("/users")
-                .then().assertThat()
-                .body("name[0]", equalTo("Leanne Graham"));
-        System.out.println("222");
+                .when().get(" /posts")
+                .then()
+                .statusCode(200);
+
     }
 }
